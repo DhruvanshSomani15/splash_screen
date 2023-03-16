0ba66f6c610a45acb6f6628ca4391c73b695d0d7
DhruvanshSomani15 <somanidhruvansh@gmail.com>
2023-03-16 17:35:03 +0530

    intial commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -21,10 +21,12 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: 900, // set the height of the container
+        width: 900, // set the width of the container
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/messi.png'),
-            fit: BoxFit.cover,
+            fit: BoxFit.contain, // set the fit of the image
           ),
         ),
       ),
